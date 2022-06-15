94ad0e755fe85a77a8267f53156c2420834341c6
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-15 20:51:35 +0200

    Update Invite.js

--- Bot Related/Invite.js
+++ Bot Related/Invite.js
@@ -4,36 +4,36 @@
                                 .setLabel('Bot Invite link')
                                 .setStyle('LINK')
                                 .setEmoji('<:GBFLogo:838990392128307231>')
-                                .setURL('https://discord.com/api/oauth2/authorize?client_id=795361755223556116&permissions=1642787765494&scope=bot%20applications.commands'),
+                                .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1642787765494&scope=bot%20applications.commands`),
                             )
                             .addComponents(new MessageButton()
                                 .setLabel('Support server')
                                 .setStyle('LINK')
                                 .setEmoji('<:LogoTransparent:838994085527945266>')
-                                .setURL('https://discord.gg/PuZMhvhRyX'),
+                                .setURL(`https://discord.gg/${code}`),
                             )
                             .addComponents(new MessageButton()
                                 .setLabel('Top.gg')
                                 .setStyle('LINK')
                                 .setEmoji('<:tog:882279600506433607>')
-                                .setURL('https://top.gg/bot/795361755223556116/vote'),
+                                .setURL(`https://top.gg/bot/795361755223556116/vote`),
                             )
                             .addComponents(new MessageButton()
                                 .setLabel('Patreon')
                                 .setStyle('LINK')
                                 .setEmoji('<:patreon:882279599394930699>')
-                                .setURL('https://www.patreon.com/GBFBot'),
+                                .setURL(`https://www.patreon.com/${name}`),
                             );
                         //Creating the invite links
                         const mainembed = new MessageEmbed()
                             .setTitle(`${emojis.VERIFY} GBF Links`)
                             .addFields({
                                 name: 'Invite me to your server!',
-                                value: (`- [Bot invite link](${'https://discord.com/api/oauth2/authorize?client_id=795361755223556116&permissions=1642787765494&scope=bot%20applications.commands'})`),
+                                value: (`- [Bot invite link](${`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1642787765494&scope=bot%20applications.commands`})`),
                                 inline: true
                             }, {
                                 name: 'Support server!',
-                                value: (`- [Support server link](${'https://discord.gg/PuZMhvhRyX'})`),
+                                value: (`- [Support server link](${`https://discord.gg/${code}`})`),
                                 inline: true
                             }, {
                                 name: "\u200b",
@@ -41,11 +41,11 @@
                                 inline: true
                             }, {
                                 name: `Vote for ${client.user.username} on Top.gg!`,
-                                value: `- [Vote here](${'https://top.gg/bot/795361755223556116/vote'})`,
+                                value: `- [Vote here](${`https://top.gg/bot/${client.user.id}/vote`})`,
                                 inline: true
                             }, {
                                 name: `Support ${client.user.username} on Patreon!`,
-                                value: `- [Patreon link](${'https://www.patreon.com/GBFBot'})`,
+                                value: `- [Patreon link](${`https://www.patreon.com/${name}`})`,
                                 inline: true
                             }, {
                                 name: "\u200b",
