7085300bd3d8be9d351b1065a36c3ee03dea25c5
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-15 03:02:02 +0200

    Update Lockdown.js

--- Channel Related/Lockdown.js
+++ Channel Related/Lockdown.js
@@ -1,20 +1,22 @@
+       //Just so I don't have to add interaction.guild everwhere
         const guild = interaction.guild;
-
+        //Getting all of the "TEXT" channels in the guild, this is here so we can avoid an errors relating to the "Locking"
         const textChannels = guild.channels.cache.filter(channel => channel.type === "GUILD_TEXT");
-
+        //Adding the channels to an array
         const textChannelsArray = [];
         await textChannels.forEach(channel => {
             textChannelsArray.push(channel);
         })
-
+        //Looping through all of the array contents using a basic for loop
         for (let i = 0; i < textChannelsArray.length; i++) {
             const channel = textChannelsArray[i];
+            //Denying the "SEND_MESSAGES" permission, "SEND_MESSAGES" is deprecated in v14, so to avoid having to change it when updating, we just use this
             channel.permissionOverwrites.set([{
                 id: interaction.guild.id,
-                deny: ['SEND_MESSAGES'],
+                deny: [Permissions.FLAGS.SEND_MESSAGES],
             }, ], `Locked down by ${interaction.user.username}`);
         }
-
+        //Creating an embed to send to the user 
         const lockedDown = new MessageEmbed()
             .setTitle(`${emojis.SUCCESS} Success`)
             .setDescription(`Locked down all channels in ${guild.name}\n\n⚠ The channels will have to be **manually** unlocked ⚠`)
