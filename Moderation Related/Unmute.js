902e854630a156e9214dffbed8cfc9b8e6ed087e
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-16 09:09:55 +0200

    🌨

---
+++ Moderation Related/Unmute.js
@@ -0,0 +1,56 @@
+        const target = interaction.options.getUser("user");
+        const muteReason = interaction.options.getString("reason") || "No reason given";
+        //Fetching the user
+        const GuildMember = interaction.guild.members.cache.get(target.id);
+
+        const noMember = new MessageEmbed()
+            .setTitle(`${emojis.ERROR} You can't do that`)
+            .setDescription(`The user you specified isn't in ${interaction.guild.name}`)
+            .setColor(colours.ERRORRED)
+            .setFooter({
+                text: `${interaction.guild.name} logging powered by GBF`,
+                iconURL: interaction.guild.iconURL()
+            })
+            .setTimestamp()
+        //If the user does not exist
+        if (!GuildMember) return interaction.reply({
+            embeds: [noMember],
+            ephemeral: true
+        })
+
+        const notMuted = new MessageEmbed()
+            .setTitle(`${emojis.ERROR} You can't do that`)
+            .setDescription(`The user you specified isn't muted.`)
+            .setColor(colours.ERRORRED)
+            .setFooter({
+                text: `${interaction.guild.name} logging powered by GBF`,
+                iconURL: interaction.guild.iconURL()
+            })
+            .setTimestamp()
+        //Checking if the user is already not muted 
+        if (GuildMember.isCommunicationDisabled() === false) return interaction.reply({
+            embeds: [notMuted],
+            ephemeral: true
+        })
+        //Un-muting the user
+        await GuildMember.timeout(0, muteReason);
+
+        const userMuted = new MessageEmbed()
+            .setTitle(`${emojis.SUCCESS} Success`)
+            .setDescription(`${target.tag} has been unmuted`)
+            .addFields({
+                name: "Reason:",
+                value: `${muteReason}`
+            }, {
+                name: "Moderator:",
+                value: `${interaction.user.tag}`
+            })
+            .setFooter({
+                text: `${interaction.guild.name} logging powered by GBF`,
+                iconURL: interaction.guild.iconURL()
+            })
+            .setTimestamp()
+
+        return interaction.reply({
+            embeds: [userMuted]
+        })
