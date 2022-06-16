a095d19710e26a5e1a60bdf3163e8a12eec35a6d
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-16 13:18:10 +0200

    🌨

--- Moderation Related/messageUpdate.js
+++ Moderation Related/messageUpdate.js
@@ -1,12 +1,13 @@
 module.exports = client => {
+    //Message update event : https://discord.js.org/#/docs/discord.js/stable/class/Client?scrollTo=e-messageUpdate
     client.on('messageUpdate', async (oldMessage, newMessage) => {
-
-        if (newMessage.author.bot || newMessage.channel.type === 'dm' || newMessage.webhookID || oldMessage.content === newMessage.content) return;
-
+           //Returning if the author is a bot, if it's a DM channel, if it's a webhook or if the content is the same
+        if (newMessage.author.bot || newMessage.channel.type === 'DM' || newMessage.webhookID || oldMessage.content === newMessage.content) return;
+        //Trimming the message if it's too long
         if (newMessage.content.length > 1024) newMessage.content = newMessage.content.slice(0, 1021) + '...';
 
         if (oldMessage.content.length > 1024) oldMessage.content = oldMessage.content.slice(0, 1021) + '...';
-
+         //Fetching data from the databse, for multi-guild bots
         let EditData = await EditLogsSchema.findOne({
             guildId: oldMessage.guild.id
         })
@@ -14,14 +15,15 @@ module.exports = client => {
         let LogsChannel = await LogSchema.findOne({
             guildId: oldMessage.guild.id
         })
-
+        //If there's no data then return
         if (!EditData || !LogsChannel) return;
 
         if (LogsChannel.Channel === null) return;
-
+        //Fetching the channel set by the user
         let editChannel = oldMessage.guild.channels.cache.get(LogsChannel.Channel)
 
         if (EditData.Enabled === true) {
+            //Checking if the channel exists, is viewable by the bot and the bot has permissions to send messages in 
             if (editChannel && editChannel.viewable && editChannel.permissionsFor(newMessage.guild.me).has(Permissions.FLAGS.SEND_MESSAGES)) {
 
                     const MessageEditEmbed = new MessageEmbed()
@@ -36,7 +38,7 @@ module.exports = client => {
                         })
                         .setColor("#e91e63")
                         .setTimestamp()
-
+                    //Creating a webhook to send to the channel
                     const WebHook = await editChannel.createWebhook(`${client.user.username} Logging 🚓`, {
                         avatar: client.user.displayAvatarURL({
                             dynamic: true
@@ -47,6 +49,7 @@ module.exports = client => {
                         await WebHook.send({
                             embeds: [MessageEditEmbed]
                         }).then(() => {
+                            //Deleting the webhook to avoid limits
                             setTimeout(() => WebHook.delete().catch(err => console.log(err)), 3000)
                         })
                     } catch (err) {
@@ -66,7 +69,7 @@ module.exports = client => {
                         return console.log(`Error while running messageUpdate: ${err}`)
                     }
 
-            } else return;
-        } else return;
+            } else return; //returning if the setting is set as false 
+        } else return; //Returning if the channel doesn't exist/isn't viewable/bot can't send messages to 
     })
 }
