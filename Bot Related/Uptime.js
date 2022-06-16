17bb0f5aef003039f8591284ef5b3c58797eb0e3
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-06-16 08:39:30 +0200

    Added UNIX support

--- Bot Related/Uptime.js
+++ Bot Related/Uptime.js
@@ -5,6 +5,10 @@
                             units: ["y", "mo", "w", "d", "h", "m", "s"],
                             round: true
                         });
+                  //If you want to use UNIX timestamp:
+                    const uptimeUNIX = (new Date() / 1000 - client.uptime / 1000).toFixed()
+                    let uptime = `<t:${uptime}:R>`
+
                       //Uptime embed
                         const UptimeEmbed = new MessageEmbed()
                             .setTitle(`${client.user.username}'s uptime`)
