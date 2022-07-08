1560149b901db8b576d0a6fb063150097da113a7
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-08 10:48:17 +0200

    Added GuildAvatar support

--- User Related/Avatar.js
+++ User Related/Avatar.js
@@ -1,31 +1,53 @@
-                        //Getting the user using slash command options
+                       //Getting the user as a User from the interaction options
                         const mentionedUser = interaction.options.getUser('user') || interaction.user;
-                        //Creating a button that takes you to their avatar URL
-                        const avatarbutton = new MessageButton()
+                        //Creating a button with the public avatar URL
+                        const avatarUserButton = new MessageButton()
                             .setStyle('LINK')
-                            .setLabel(`${mentionedUser.username}'s avatar link`)
+                            .setLabel(`Public Avatar link`)
                             .setURL(`${mentionedUser.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
-
-                        const row = new MessageActionRow().addComponents([avatarbutton])
-                         //The embed
-                        const avatarembed = new MessageEmbed()
+                        //Creating an embed that shows the data
+                        const avatarEmbed = new MessageEmbed()
                             .setTitle(`${mentionedUser.tag}'s avatar`)
-                             //Hyper link to the avatar URL
-                            .setDescription(`[Avatar URL](${mentionedUser.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })})`)
+                            .setDescription(`[Avatar URL](${mentionedUser.displayAvatarURL({ dynamic: true, size: 1024 })})`)
                             .setColor("#e91e63")
                             .setFooter({
                                 text: `Requested By: ${interaction.user.tag}`,
                                 iconURL: `${interaction.user.displayAvatarURL()}`
                             })
-                             //Displaying the avatar
                             .setImage(mentionedUser.displayAvatarURL({
                                 format: 'png',
                                 dynamic: true,
                                 size: 1024
                             }))
-
+                        //Attempting to fetch the member, you can also use guild.members.cache.get(mentionedUser.id)
+                        const fetchedMember = interaction.guild.members.fetch(mentionedUser.id);
+                        //Creating an empty button that we will add properties to later
+                        const avatarMemberButton = new MessageButton()
+                        //If the user is a GuildMember and their server avatar is different from the public avatar
+                        if (fetchedMember && (mentionedUser.displayAvatarURL() !== fetchedMember.displayAvatarURL())) {
+                            //Setting the properties to the empty button
+                            avatarMemberButton.setStyle('LINK')
+                            avatarMemberButton.setLabel(`Server Avatar link`)
+                            avatarMemberButton.setURL(fetchedMember.displayAvatarURL({
+                                dynamic: true
+                            }))
+                            //Updating the embed to also show the guild avatar
+                            avatarEmbed.setDescription(`[Avatar URL](${mentionedUser.displayAvatarURL({ dynamic: true, size: 1024 })})\n[Server Avatar URL](${fetchedMember.displayAvatarURL({ dynamic: true, size: 1024})})`)
+                            avatarEmbed.setThumbnail(fetchedMember.displayAvatarURL({
+                                dynamic: true
+                            }))
+                        //If the IF statement returns false
+                        } else {
+                            //Setting the button as a disabled button with some random URL
+                            avatarMemberButton.setStyle("LINK")
+                            avatarMemberButton.setLabel("Server Avatar")
+                            avatarMemberButton.setDisabled(true)
+                            avatarMemberButton.setURL(`https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png`)
+                        }
+                        //Creating the message action row that stores all the buttons
+                        const buttonRows = new MessageActionRow().addComponents([avatarUserButton, avatarMemberButton])
+                        //Sending the embed
                         return interaction.reply({
-                            embeds: [avatarembed],
-                            components: [row]
+                            embeds: [avatarEmbed],
+                            components: [buttonRows]
                         })
-                    }
