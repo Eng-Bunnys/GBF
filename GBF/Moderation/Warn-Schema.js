8e10d1ea47d10798823181b646596b8f8ff5fa59
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-13 16:37:38 +0200

    Moderation Warn Schema

---
+++ GBF/Moderation/Warn-Schema.js
@@ -0,0 +1,16 @@
+const {
+    Schema,
+    model
+} = require('mongoose');
+
+const UserWarnsSchema = new Schema({
+    guildId: String,
+    userId: String,
+    warns: Number,
+    warnID: Array,
+    warnReason: Array,
+}, {
+    collection: 'User Warns Docs'
+})
+
+module.exports = model('User Warns Docs', UserWarnsSchema)
