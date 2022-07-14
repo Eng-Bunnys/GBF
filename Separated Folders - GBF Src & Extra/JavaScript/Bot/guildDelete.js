f0d8fd07d50996820e768f9d29268a4d6ac7b989
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-14 23:19:07 +0200

    [JS] guildDelete Event || Bot Leave
    Emitted when your client leaves a guild/it is deleted

---
+++ Separated Folders - GBF Src & Extra/JavaScript/Bot/guildDelete.js
@@ -0,0 +1,23 @@
+module.exports = (client) => {
+    //Guild delete event : Emitted when a server is deleted/bot is kicked from it : https://discord.js.org/#/docs/discord.js/stable/class/Client?scrollTo=e-guildDelete
+    client.on("guildDelete", async (guild) => {
+
+        const kickedOrDelete = new MessageEmbed()
+            .setTitle(`${emojis.ERROR} I've been kicked`)
+            .setDescription(`I've been kicked from ${guild.name} (${guild.id})\nMembers: ${guild.memberCount}`)
+            .setColor(colours.ERRORRED)
+            .setTimestamp()
+        //Sending a message to a developer channel to inform the developers
+        const devChannel = await client.channels.fetch("ID").catch(() => null)
+
+        devChannel.send({
+            embeds: [kickedOrDelete]
+        })
+        //Telling the developer
+        devChannel.send({
+            content: `<@ID> I have been removed from ${guild.name}`
+        })
+        //Adding the info in a console
+        console.log(redBright(`I have been removed from ${guild.name}, it has ${guild.memberCount} members`))
+    })
+}
