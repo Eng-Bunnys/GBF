406c812e3412271081a9b4b8fd83abe61bfe13d4
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-23 18:07:38 +0200

    Added mongoDB support

---
+++ GBF Handler/New/index.js
@@ -0,0 +1,20 @@
+const GBFHandler = require("./handler/src");
+const { Client, Intents } = require("discord.js");
+const path = require("path");
+require("dotenv/config");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.on("ready", () => {
+  console.log(`${client.user?.username} is now online!`);
+});
+
+new GBFHandler({
+  client,
+  mongoURI: process.env.MONGO_URI,
+  commandsDir: path.join(__dirname, "commands"),
+})
+
+client.login(process.env.TOKEN);
