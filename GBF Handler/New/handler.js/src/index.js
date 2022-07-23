0e0fa3ee0f603973dbcefc418b4b31cb26d123a8
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-23 18:11:06 +0200

    Added mongoDB connection

---
+++ GBF Handler/New/handler.js/src/index.js
@@ -0,0 +1,19 @@
+const GBFHandler = require("./Command Handler/mainHandler");
+const mongoose = require("mongoose");
+
+class GBF {
+  constructor({ client, mongoURI, commandsDir }) {
+    if (!client) throw new Error(`A client is required.`);
+    if (commandsDir) new GBFHandler(commandsDir, client);
+
+    if (mongoURI) this.mongoConnect(mongoURI);
+  }
+
+  mongoConnect(mongoURI) {
+    mongoose.connect(mongoURI, {
+      keepAlive: true,
+    });
+  }
+}
+
+module.exports = GBF;
