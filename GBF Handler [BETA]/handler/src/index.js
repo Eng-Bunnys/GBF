aba8febe1da49881785bb53fe8719663623d02c5
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-23 17:40:42 +0200

    Added MongoDB support

--- GBF Handler [BETA]/handler/src/index.js
+++ GBF Handler [BETA]/handler/src/index.js
@@ -1,10 +1,19 @@
 const GBFHandler = require("./mainHandler");
+const mongoose = require("mongoose");
 
 class GBF {
-    constructor({ client, commandsDir }) {
-        if (!client) throw new Error(`You need to pass in "client" in the main file`);
-        if (commandsDir) new GBFHandler(commandsDir, client);
-    }
+  constructor({ client, mongoURI, commandsDir }) {
+    if (!client) throw new Error(`A client is required.`);
+    if (commandsDir) new GBFHandler(commandsDir, client);
+
+    if (mongoURI) this.mongoConnect(mongoURI);
+  }
+
+  mongoConnect(mongoURI) {
+    mongoose.connect(mongoURI, {
+      keepAlive: true,
+    });
+  }
 }
 
 module.exports = GBF;
