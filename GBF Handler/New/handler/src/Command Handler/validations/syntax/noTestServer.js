20f1a87c92827f8901ad0d752c1c21deddfd6309
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-25 15:05:55 +0200

    Re-doing the handler

--- GBF Handler/New/handler/src/Command Handler/validations/syntax/noTestServer.js
+++
@@ -1,7 +0,0 @@
-module.exports = (command) => {
-  const { instance, commandName, commandObject } = command;
-
-  if (commandObject.testOnly !== true || instance.testServers.length) return;
-
-  throw new Error(`There are no test servers specified and the command "${commandName}" is a test only command.`);
-};
--- GBF Handler/New/handler/src/Command Handler/validations/syntax/requiredCallback.js
+++
@@ -1,6 +0,0 @@
-module.exports = (command) => {
-  const { commandName, commandObject } = command;
-
-  if (!commandObject.callback)
-    throw new Error(`A callback function is required to run the command "${commandName}"`);
-};
