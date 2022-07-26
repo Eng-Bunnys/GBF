7ed0d47c72ff0909359bbe3a116b5b41a77811de
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-26 23:22:55 +0200

    [#R2] Argument Count Checker

---
+++ GBF Handler/handler/main/validations/run-time/2-argCounter.js
@@ -0,0 +1,27 @@
+const errMsgs = require("../../../../settings/errorMessages.json");
+module.exports = (command, usage, prefix) => {
+  console.log(`Argument count check running`);
+  const { minArgs = 0, maxArgs = -1, correctSyntax } = command.commandObject;
+  const { length } = usage.args;
+
+  if (length < minArgs) {
+    usage.message.reply({
+      content: `${errMsgs.MIN_ARGS.replace("{ArgCount}", minArgs).replace(
+        "{Syntax}",
+        correctSyntax.replace("{PREFIX}", prefix)
+      )}`,
+    });
+    return false;
+  }
+
+  if (length > maxArgs && maxArgs !== -1) {
+    usage.message.reply({
+      content: `${errMsgs.MAX_ARGS.replace("{ArgCount}", maxArgs).replace(
+        "{Syntax}",
+        correctSyntax.replace("{PREFIX}", prefix)
+      )}`,
+    });
+    return false;
+  }
+  return true;
+};
