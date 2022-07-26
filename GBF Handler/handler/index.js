0295e4cede374ad4cda039bf1ff0c6ae0246fbac
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-26 22:38:40 +0200

    Added support for test servers and mongoDB

--- GBF Handler/handler/index.js
+++ GBF Handler/handler/index.js
@@ -1,1 +1,31 @@
-console.log(`Handler is now online`);
+const CommandHandler = require('./main/MainHandler');
+
+const mongoose = require('mongoose');
+
+class GBF {
+  constructor({ client, mongoURI, commandsDir, testServers = [] }) {
+    if (!client) throw new Error('You must provide a client instance in the handler setup.')
+
+    this._testServers = testServers;
+
+    if (mongoURI) {
+      this.mongoConnect(mongoURI)
+    }
+
+    if (commandsDir) {
+      new CommandHandler(this, commandsDir, client);
+    }
+  }
+
+  get testServers() {
+    return this._testServers
+  }
+
+  mongoConnect(mongoURI) {
+    mongoose.connect(mongoURI, {
+      keepAlive: true,
+    })
+  }
+}
+
+module.exports = GBF;
