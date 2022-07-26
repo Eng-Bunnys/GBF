169a1b924c43d4c5482ee338be34fd066407b0c9
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-26 22:52:56 +0200

    Updated to support Guild Commands

---
+++ GBF Handler/handler/main/MainHandler.js
@@ -0,0 +1,78 @@
+const getAllFiles = require("../util/engine");
+const Command = require("./Command");
+
+const path = require("path");
+
+class GBFHandler {
+  commands = new Map();
+
+  constructor(instance, commandsDir, client) {
+    this._instance = instance;
+    this._commandsDir = commandsDir;
+    this.readFiles();
+    this.messageListener(client);
+  }
+
+  readFiles() {
+    const files = getAllFiles(this._commandsDir);
+    const validations = this.getValidations("syntax");
+
+    for (let file of files) {
+      const commandObject = require(file);
+
+      console.log(commandObject);
+
+     // const commandName = commandObject.name.toLowerCase(); 
+
+     let commandName = file.split(/[/\\]/).pop().split(".")[0];
+
+      const command = new Command(this._instance, commandName, commandObject);
+
+      for (const validation of validations) {
+        validation(command);
+      }
+
+      this.commands.set(command.commandName, command);
+    }
+  }
+
+  messageListener(client) {
+    const validations = this.getValidations("run-time");
+
+    const prefix = "!";
+
+    client.on("messageCreate", (message) => {
+      if (!message.content.startsWith(prefix)) return;
+
+      const args = message.content.split(/\s+/);
+      const commandName = args.shift().substring(prefix.length).toLowerCase();
+
+      const command = this.commands.get(commandName);
+      if (!command) return;
+
+      const usage = {
+        message,
+        args,
+        text: args.join(" "),
+        guild: message.guild,
+      };
+
+      for (const validation of validations) {
+        console.log(validation(command, usage, prefix));
+        if (!validation(command, usage, prefix)) continue;
+      }
+
+      const { callback } = command.commandObject;
+      callback(usage);
+    });
+  }
+
+  getValidations(folder) {
+    const validations = getAllFiles(
+      path.join(__dirname, `./validations/${folder}`)
+    ).map((filePath) => require(filePath));
+    return validations;
+  }
+}
+
+module.exports = GBFHandler;
