553b4d12b81eb6e6a6c21523d2642ff9ed45e3bd
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-26 22:38:09 +0200

    Added options that the handler supports

---
+++ GBF Handler/commands/add.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //name: "add",
+  minArgs: 2, //default: 0
+  maxArgs: 3, //default: -1 (infinite)
+  correctSyntax: "Correct syntax: {PREFIX}add <num 1> <num 2> <etc>",
+
+  testOnly: true, //boolean (default: false)
+
+  callback: ({ message, args }) => {
+    let sum = 0;
+
+    for (const arg of args) sum += parseInt(arg);
+
+    message.reply({
+      content: `The sum is ${sum}`,
+    });
+  },
+};
