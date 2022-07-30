5f874455ce3daf4393c366eb21e8a84eb23392c0
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-30 21:16:44 +0200

    Updated to use the new features

--- GBF Handler/commands/add.js
+++ GBF Handler/commands/add.js
@@ -1,20 +1,29 @@
 module.exports = {
-  name: "test", //Will always be lowercase, this feature is here to avoid slash command errors
-  description: "Adds two numbers together", //Command description, required for slash commands
-  type: "LEGACY", //LEGACY : Prefix Commands, SLASH : Slash Commands, BOTH : Both types
-  minArgs: 2, //default: 0 [No support slash rn]
-  maxArgs: 3, //default: -1 (infinite) [No support slash rn]
-  correctSyntax: "Correct syntax: {PREFIX}add <num 1> <num 2> <etc>", //[No support slash rn]
+  name: "add", //Will always be lowercase, this feature is here to avoid slash command errors
+  description: "Adds two numbers together",
+  type: "BOTH", //LEGACY, SLASH, BOTH
+  minArgs: 2, //default: 0
+  maxArgs: 3, //default: -1 (infinite)
+  correctSyntax: "Correct syntax: {PREFIX}add <num 1> <num 2> <etc>",
 
-  testOnly: true, //boolean (default: false)
-  //We will change this to use interaction once a slash listener is made
-  callback: ({ message, args }) => {
+  testOnly: true, //boolean (default: false) || development only
+
+  callback: ({ interaction, message, args }) => {
     let sum = 0;
 
     for (const arg of args) sum += parseInt(arg);
 
-    message.reply({
-      content: `The sum is ${sum}`,
-    });
+    if (message) {
+       message.reply({
+          content: `Message: The sum is ${sum}`,
+      })
+    } else {
+      interaction.reply({
+          content: `Interaction: The sum is ${sum}`,
+      })
+    }
+
+   // return `The sum is ${sum}`; //This makes it so the handler handles the return and we don't have to
+   //check if it's a message or interaction, you don't even have to define them
   },
 };
