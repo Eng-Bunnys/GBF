4ab287dd0124bb510920f0ce269f1a137316f3b0
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-31 22:46:45 +0200

    Added expectedArg options creation

--- GBF Handler/handler/main/SlashHandler.js
+++ GBF Handler/handler/main/SlashHandler.js
@@ -1,3 +1,5 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+
 class GBFSlash {
   constructor(client) {
     this._client = client;
@@ -22,9 +24,7 @@ class GBFSlash {
     const commands = await this.getCommands(guildId);
 
     const existingCommand = commands.cache.find((cmd) => cmd.name === name);
-    if (existingCommand) {
-      return console.log(`Command "${name}" because it already exists`);
-    }
+    if (existingCommand) return console.log(`Ignoring the command "${name}" because it already exists`);
 
     await commands.create({
       name,
@@ -43,6 +43,26 @@ class GBFSlash {
     if (!existingCommand) return;
 
     await existingCommand.delete();
+  }
+
+  creatOptions({ expectedArgs = "", minArgs = 0 }) {
+    const options = [];
+    if (expectedArgs) {
+      const split = expectedArgs.substring(1, expectedArgs.length - 1)
+      .split(/[>\]] [<\[]/);
+
+      for (let i = 0; i < split.length; i++) {
+        const arg = split[i];
+        options.push({
+          name: arg.toLowerCase().replace(/\s+/g, '-'),
+          description: arg,
+          type: ApplicationCommandOptionType.String,
+          required: i < minArgs,
+        })
+      }
+      console.log(options);
+    }
+     return options;
   }
 }
 
