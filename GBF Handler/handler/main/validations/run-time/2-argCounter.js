b79c7b3e0cf570e4ed06c61d4b47b342f6196216
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-07-31 22:57:26 +0200

    [#5] Added a check for interaction & new args

--- GBF Handler/handler/main/validations/run-time/2-argCounter.js
+++ GBF Handler/handler/main/validations/run-time/2-argCounter.js
@@ -1,25 +1,47 @@
 const errMsgs = require("../../../../settings/errorMessages.json");
 module.exports = (command, usage, prefix) => {
-  const { minArgs = 0, maxArgs = -1, correctSyntax } = command.commandObject;
+  const {
+    minArgs = 0,
+    maxArgs = -1,
+    correctSyntax,
+    expectedArgs = "",
+  } = command.commandObject;
   const { length } = usage.args;
+  const { message, interaction } = usage;
 
   if (length < minArgs) {
-    usage.message.reply({
-      content: `${errMsgs.MIN_ARGS.replace("{ArgCount}", minArgs).replace(
-        "{Syntax}",
-        correctSyntax.replace("{PREFIX}", prefix)
-      )}`,
-    });
+    const lowerText = `${errMsgs.MIN_ARGS.replace("{ArgCount}", minArgs)
+      .replace("{ARGS}", expectedArgs)
+      .replace("{Syntax}", correctSyntax.replace("{PREFIX}", prefix))}`;
+
+    if (message)
+      message.reply({
+        content: lowerText,
+      });
+    else if (interaction)
+      interaction.reply({
+        content: lowerText,
+        ephemeral: true,
+      });
+
     return false;
   }
 
   if (length > maxArgs && maxArgs !== -1) {
-    usage.message.reply({
-      content: `${errMsgs.MAX_ARGS.replace("{ArgCount}", maxArgs).replace(
-        "{Syntax}",
-        correctSyntax.replace("{PREFIX}", prefix)
-      )}`,
-    });
+    const higherText = `${errMsgs.MAX_ARGS.replace("{ArgCount}", maxArgs)
+      .replace("{ARGS}", expectedArgs)
+      .replace("{Syntax}", correctSyntax.replace("{PREFIX}", prefix))}`;
+
+    if (message)
+      message.reply({
+        content: higherText,
+      });
+    else if (interaction)
+      interaction.reply({
+        content: higherText,
+        ephemeral: true,
+      });
+
     return false;
   }
   return true;
