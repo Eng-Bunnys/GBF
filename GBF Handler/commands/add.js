710b4c5d55746e4e70db3dcf662ce8c4681a531d
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-08-01 18:13:39 +0200

    Added devOnly boolean

--- GBF Handler/commands/add.js
+++ GBF Handler/commands/add.js
@@ -2,7 +2,7 @@ const { ApplicationCommandOptionType } = require("discord.js");
 
 module.exports = {
   name: "add", //Will always be lowercase, this feature is here to avoid slash command errors
-  description: "Adds two numbers together",
+  description: "Adds two numbers together!",
   type: "BOTH", //LEGACY, SLASH, BOTH
 
   minArgs: 2, //default: 0
@@ -10,9 +10,9 @@ module.exports = {
 
   correctSyntax: "Correct syntax: {PREFIX}add {ARGS}",
   //expectedArgs will create slash command options but they will be alot less descriptive than if you specified
-  //with options as shown in line below, it is recommended to use options instead of expectedArgs 
+  //with options as shown in line below, it is recommended to use options instead of expectedArgs
   //so this should only be used in small commands and will always be type: STRING
-  expectedArgs: '<number> <number 2>', //Must be matching syntax of <> or [], will not work if <number] etc.
+  expectedArgs: "<number> <number 2>", //Must be matching syntax of <> or [], will not work if <number] etc.
   //expectedArgs will look something like this:
   /*
   expectedArgs: '<option> <option 2>',
@@ -24,20 +24,25 @@ module.exports = {
   */
 
   //The options used in the **slash** command
-   options: [{
-   name: 'first-number',
-    description: "The first number to add",
-   type: ApplicationCommandOptionType.Number,
-   required: true
-   }, {
-     name: 'second-number',
-     description: "The second number to add",
-     type: ApplicationCommandOptionType.Number,
-     required: true
-   }],
+  options: [
+    {
+      name: "first-number",
+      description: "The first number to add",
+      type: ApplicationCommandOptionType.Number,
+      required: true,
+    },
+    {
+      name: "second-number",
+      description: "The second number to add",
+      type: ApplicationCommandOptionType.Number,
+      required: true,
+    },
+  ],
 
-  testOnly: true, //boolean (default: false) || development only
-  disabled: false, //This will delete/disable the command if true (default: false) so it won't show in slash or legacy 
+  testOnly: false, //boolean (default: false) || development only, will only work in test servers
+  devOnly: true, //boolean (default: false) || will only work with bot owners
+  
+  disabled: false, //This will delete/disable the command if true (default: false) so it won't show in slash or legacy
 
   callback: ({ interaction, message, args }) => {
     let sum = 0;
@@ -45,16 +50,16 @@ module.exports = {
     for (const arg of args) sum += parseInt(arg);
 
     if (message) {
-       message.reply({
-          content: `Message: The sum is ${sum}`,
-      })
+      message.reply({
+        content: `Message: The sum is ${sum}`,
+      });
     } else {
       interaction.reply({
-          content: `Interaction: The sum is ${sum}`,
-      })
+        content: `Interaction: The sum is ${sum}`,
+      });
     }
 
-   // return `The sum is ${sum}`; //This makes it so the handler handles the return and we don't have to
-   //check if it's a message or interaction, you don't even have to define them
+    // return `The sum is ${sum}`; //This makes it so the handler handles the return and we don't have to
+    //check if it's a message or interaction, you don't even have to define them
   },
 };
