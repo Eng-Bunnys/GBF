b68f7be6b986b60e90e3206789cfba1b42e272b5
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-08-08 17:24:41 +0200

    Added API error embed

--- Utility/GBFEmbeds.js
+++ Utility/GBFEmbeds.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const colours = require('../GBFColor.json');
+const colours = require("../GBFColor.json");
 
 const ImageGenerating = new MessageEmbed()
   .setTitle(`Generating image... <a:Loading:971730094169141248>`)
@@ -9,6 +9,22 @@ const ImageGenerating = new MessageEmbed()
   )
   .setTimestamp();
 
+const APIError = new MessageEmbed()
+  .setTitle(titles.ERROR)
+  .setDescription(
+    `An error in the API occured, I've already reported it to my developers!\nPlease try again later.\n\nError:\`\`\`js\n${err}\`\`\``
+  )
+  .setColor(colours.ERRORRED)
+  .addFields({
+    name: "FYI:",
+    value: `\`User aborted the request\` error just means that the bot is under load`,
+  })
+  .setFooter({
+    text: `We apologize for the inconvenience`,
+    iconURL: client.user.displayAvatarURL(),
+  });
+
 module.exports = {
   ImageGenerating,
+  APIError,
 };
