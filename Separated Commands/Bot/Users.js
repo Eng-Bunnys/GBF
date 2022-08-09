db82bee2884e59f452499288a53f8275a6a997b4
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-08-09 06:03:50 +0200

    Improved total users accuracy using math

---
+++ Separated Commands/Bot/Users.js
@@ -0,0 +1,56 @@
+let totalNumberOfUsers = [];
+await client.guilds.cache.map(guild => {
+    totalNumberOfUsers.push(guild.memberCount);
+    return totalNumberOfUsers
+});
+totalNumberOfUsers = totalNumberOfUsers.reduce((a, b) => a + b, 0);
+
+const totalServers = client.guilds.cache.size;
+
+const averageUsers = (totalNumberOfUsers / totalServers).toFixed(0);
+
+const displayButtons = new MessageActionRow()
+    .addComponents(
+        new MessageButton()
+        .setLabel(`${totalNumberOfUsers.toLocaleString()} users`)
+        .setStyle("SECONDARY")
+        .setDisabled(true)
+        .setCustomId(`totalUsersDisabled`),
+        new MessageButton()
+        .setLabel(`${totalServers.toLocaleString()} servers`)
+        .setStyle("SECONDARY")
+        .setDisabled(true)
+        .setCustomId(`totalServersDisabled`),
+        new MessageButton()
+        .setLabel(`${averageUsers.toLocaleString()} average users per server`)
+        .setStyle("SECONDARY")
+        .setDisabled(true)
+        .setCustomId(`averageUsersDisabled`)
+    )
+
+const usersEmbed = new MessageEmbed()
+    .setTitle(`${client.user.username}'s user stats`)
+    .addFields({
+        name: 'Total users',
+        value: `${totalNumberOfUsers.toLocaleString()}`,
+        inline: true
+    }, {
+        name: 'Total servers',
+        value: `${totalServers.toLocaleString()}`,
+        inline: true
+    }, {
+        name: 'Average users per server',
+        value: `${averageUsers.toLocaleString()}`,
+        inline: true
+    })
+    .setColor(colours.DEFAULT)
+    .setFooter({
+        text: `Requested by: ${interaction.user.username}`,
+        iconURL: interaction.user.displayAvatarURL()
+    })
+    .setTimestamp()
+
+return interaction.reply({
+    embeds: [usersEmbed],
+    components: [displayButtons]
+});
