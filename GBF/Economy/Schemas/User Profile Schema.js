c8a166645f0004ba7abf8ac54b7f02fe2f7f1c89
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-09-22 16:38:40 +0200

    UserProfile schema

---
+++ GBF/Economy/Schemas/User Profile Schema.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require("mongoose");
+
+const UserProfileSchema = new Schema(
+  {
+    userId: String,
+    userName: String,
+    userNameInsensitive: String,
+    accountPassword: String,
+    wallet: {
+      type: Number,
+      default: 500
+    },
+    bank: {
+      type: Number,
+      default: 0
+    },
+    netWorth: {
+      type: Number,
+      default: 500
+    },
+    DunkelCoins: {
+      type: Number,
+      default: 250
+    },
+    totalEarned: {
+      type: Number,
+      default: 500
+    },
+    rank: {
+      type: Number,
+      default: 1
+    },
+    RP: {
+      type: Number,
+      default: 0
+    },
+    totalRPEarned: {
+      type: Number,
+      default: 0
+    },
+    accountAge: {
+      type: Date,
+      default: Date.now()
+    },
+    dailyCooldown: {
+      type: Date,
+      default: new Date(Date.now() - 21600000)
+    },
+    dailyStreak: {
+      type: Number,
+      default: 0
+    },
+    lastTransfer: {
+      type: Date,
+      default: new Date(Date.now() - 3 * 7 * 24 * 60 * 60 * 1000)
+    },
+    lastUsernameChange: {
+      type: Date,
+      default: new Date(Date.now() - 4 * 7 * 24 * 60 * 60 * 1000)
+    }
+  },
+  {
+    collection: "DunkelLuz User Profile"
+  }
+);
+
+module.exports = model("DunkelLuz User Profile", UserProfileSchema);
