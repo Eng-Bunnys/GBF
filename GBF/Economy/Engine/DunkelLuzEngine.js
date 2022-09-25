f8b31bd757bd0d085c35b176a912a9f4b83261e1
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-09-25 06:21:38 +0200

    Added daily functions & embeds

--- GBF/Economy/Engine/DunkelLuzEngine.js
+++ GBF/Economy/Engine/DunkelLuzEngine.js
@@ -1,3 +1,9 @@
+const { MessageEmbed } = require("discord.js");
+
+const colours = require("../GBFColor.json");
+const emojis = require("../GBFEmojis.json");
+const title = require("../gbfembedmessages.json");
+
 function RPRequiredToLevelUp(rank) {
   return rank * 800 + (rank - 1) * 400;
 }
@@ -22,9 +28,46 @@ function DunkelCoinsEarned(rank) {
   else return 0;
 }
 
+function DailyMoney(streak) {
+  let RewardedMoney = Math.round(streak * 300);
+
+  if (RewardedMoney >= 30000) RewardedMoney = 30000;
+  return RewardedMoney;
+}
+
+function DailyRP(streak) {
+  let RewardedRP = Math.round(streak * 100);
+
+  if (RewardedRP >= 10000) RewardedRP = 10000;
+  return RewardedRP;
+}
+
+const accountRequired = new MessageEmbed()
+  .setTitle(`${emojis.ERROR} Not yet!`)
+  .setColor(colours.ERRORRED)
+  .setDescription(
+    `A DunkelLuz account is required to use this feature, you can create one for free using \`/account login\` or transfer an existing account to this Discord account using the same command.`
+  )
+  .setFooter({
+    text: `This system is in place to help protect your progress in-case you lost your Discord account or moved to a new one`
+  })
+  .setTimestamp();
+
+const incompleteTutorial = new MessageEmbed()
+  .setTitle(`${emojis.ERROR} Not yet!`)
+  .setColor(colours.ERRORRED)
+  .setDescription(
+    `You are required to complete the DunkelLuz tutorial before using it's feautres.\n\n\`/tutorial\``
+  )
+  .setTimestamp();
+
 module.exports = {
   RPRequiredToLevelUp,
   LevelUpReward,
   checkRank,
-  DunkelCoinsEarned
+  DunkelCoinsEarned,
+  DailyMoney,
+  DailyRP,
+  accountRequired,
+  incompleteTutorial
 };
