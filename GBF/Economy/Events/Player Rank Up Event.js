36bdfa5f94edc4c71205f16cc34116104ca1f6c5
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-09-28 11:07:52 +0200

    Updated to use new system

--- GBF/Economy/Events/Player Rank Up Event.js
+++ GBF/Economy/Events/Player Rank Up Event.js
@@ -8,84 +8,105 @@ const UserProfileSchema = require("../../schemas/Economy Schemas/User Profile Sc
 const {
   checkRank,
   LevelUpReward,
-  DunkelCoinsEarned
+  DunkelCoinsEarned,
+  abbreviateNumber
 } = require("../../utils/DunkelLuzEngine");
 
 module.exports = (client) => {
-  client.on("playerLevelUp", async (interaction, player) => {
-    const userData = await UserProfileSchema.findOne({
-      userId: player.id
-    });
-
-    const noData = new MessageEmbed()
-      .setTitle(`${emojis.ERROR} Oops`)
-      .setColor(colours.ERRORRED)
-      .setDescription(
-        `We couldn't find any data on you, please contact support if this issue is repeated`
-      );
-
-    if (!userData)
-      return interaction.followUp({
-        embeds: [noData],
-        ephemeral: true
+  client.on(
+    "playerLevelUp",
+    async (interaction, player, extraLevels, remainingRP) => {
+      const userData = await UserProfileSchema.findOne({
+        userId: player.id
       });
 
-    const rankedUp = new MessageEmbed()
-      .setTitle(`${emojis.VERIFY} Ranked Up`)
-      .setColor(colours.DunkelLuzGreen)
-      .setDescription(`Current Rank: ${(userData.rank + 1).toLocaleString()}`)
-      .setFooter({
-        text: `The higher your rank, the better the rewards you get`
-      });
+      const noData = new MessageEmbed()
+        .setTitle(`${emojis.ERROR} Oops`)
+        .setColor(colours.ERRORRED)
+        .setDescription(
+          `We couldn't find any data on you, please contact support if this issue is repeated`
+        );
 
-    const cashEarned = LevelUpReward(userData.rank + 1);
-    const coinsEarned = DunkelCoinsEarned(userData.rank + 1);
+      if (!userData)
+        return interaction.followUp({
+          embeds: [noData],
+          ephemeral: true
+        });
 
-    if (userData.rank + 1 === 5000) {
-      const achievementType = {
-        type: "MaxRank",
-        name: "Max Rank",
-        requirements: `Reach Rank 5,000`,
-        hasBadge: true
-      };
-      await client.emit(
-        "achievementComplete",
-        interaction,
-        player,
-        achievementType
-      );
-    }
+      const userRankAfterOneRank = userData.rank + 1;
 
-    if (userData.rank + 1 === 100) {
-      const achievementType = {
-        type: "Rank100",
-        name: "Rank 100 💯",
-        requirements: `Reach Rank 100\n\nRewards:\n• ₲100,000\n• 5,000 Rank\n• ${emojis.dunkelCoin}10 DunkelCoins`,
-        hasBadge: false
-      };
-      await client.emit(
-        "achievementComplete",
-        interaction,
-        player,
-        achievementType
-      );
-    }
+      const rankedUp = new MessageEmbed()
+        .setTitle(`${emojis.VERIFY} Ranked Up`)
+        .setColor(colours.DunkelLuzGreen)
+        .setDescription(
+          `Current Rank: ${(
+            userRankAfterOneRank + extraLevels
+          ).toLocaleString()} ${
+            extraLevels !== 0
+              ? `|| Went up ${extraLevels.toLocaleString()} levels`
+              : ""
+          }`
+        )
+        .setFooter({
+          text: `Carry over RP: ${remainingRP.toLocaleString()} || You get DunkelCoins every 10 levels!`
+        });
 
-    if (checkRank(userData.rank, userData.RP)) {
+      const cashEarned = LevelUpReward(userRankAfterOneRank, extraLevels);
+      const coinsEarned = DunkelCoinsEarned(userRankAfterOneRank, extraLevels);
+
+      if (
+        userRankAfterOneRank + extraLevels === 5000 &&
+        !userData.achievements.includes("MaxRank")
+      ) {
+        const achievementType = {
+          type: "MaxRank",
+          name: "Max Rank",
+          requirements: `Reach Rank 5,000`,
+          hasBadge: true
+        };
+        await client.emit(
+          "achievementComplete",
+          interaction,
+          player,
+          achievementType
+        );
+      }
+
+      if (
+        userRankAfterOneRank + extraLevels >= 100 &&
+        !userData.achievements.includes("Rank100")
+      ) {
+        const achievementType = {
+          type: "Rank100",
+          name: "Rank 100 💯",
+          requirements: `Reach Rank 100\n\nRewards:\n• ₲100,000\n• 5,000 RP\n• ${emojis.dunkelCoin}10 DunkelCoins`,
+          hasBadge: false
+        };
+        await client.emit(
+          "achievementComplete",
+          interaction,
+          player,
+          achievementType
+        );
+      }
+
       rankedUp.addFields({
         name: "Rewards:",
-        value: `• ₲${cashEarned.toLocaleString()}${
+        value: `• ₲${abbreviateNumber(cashEarned, 2, false, false)}${
           coinsEarned !== 0
-            ? `\n• ${
-                emojis.DunkelCoins
-              } ${coinsEarned.toLocaleString()} DunkelCoins`
+            ? `\n• ${emojis.dunkelCoin} ${abbreviateNumber(
+                coinsEarned,
+                2,
+                false,
+                false
+              )} DunkelCoins`
             : ""
         }`
       });
 
       await userData.updateOne({
-        RP: 0,
-        rank: userData.rank + 1,
+        RP: remainingRP,
+        rank: userData.rank + 1 + extraLevels,
         bank: userData.bank + cashEarned,
         netWorth: userData.netWorth + cashEarned,
         totalEarned: userData.totalEarned + cashEarned,
@@ -97,5 +118,5 @@ module.exports = (client) => {
         embeds: [rankedUp]
       });
     }
-  });
+  );
 };
