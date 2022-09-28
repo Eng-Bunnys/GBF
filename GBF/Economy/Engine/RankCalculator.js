ac7b30497022db7f6b9b9d9fe8a1bc6d121cbe14
DepressedBunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-09-28 10:43:20 +0200

    Fixed major bugs

--- GBF/Economy/Engine/RankCalculator.js
+++ GBF/Economy/Engine/RankCalculator.js
@@ -1,42 +1,36 @@
-const { cyanBright, redBright, blueBright } = require("chalk");
+let currentRank = 1;
+let currentRP = 500;
+let earnedRP = 300000;
 
-let currentRank = 6; //User's current rank
-let rpBeforeGain = 500; //User's RP before the addition
-let gainedRP = 12000; //The amount of RP gained
-let rpAfterGain = gainedRP + rpBeforeGain; //The user's RP after the gained has been added
-
-let rankedLevels = 0; //The number of extra levels the user will get depending on how many times they rank up
-
-//Function that will calculate the amount of RP required to rank up once, uses the before gain RP
-function RPRequiredToLevelUp(rank, currentRP) {
-  return rank * 800 + (rank - 1) * 400 + currentRP;
-}
-//Function that will calculate the amount of RP required to rank up depending on how many extra ranks are added
-function RPForMultipleLevels(rank, currentRP, extraRanks) {
-  const addedRanks = rank + extraRanks;
-  return addedRanks * 800 + (addedRanks - 1) * 400 + currentRP;
+function RPRequiredToLevelUp(rank, rp) {
+  const fOfx = rank * 800 + (rank - 1) * 400;
+  return fOfx + rp;
 }
 
-let RPRequired = RPForMultipleLevels(currentRank, rpBeforeGain, rankedLevels);
-
-if (rpAfterGain === RPRequired) {
-  rankedLevels++;
-  console.log(`RP is equal and no remainder`);
+function percentageRemaing(currentRP, requiredRP) {
+  return `${Math.round((currentRP / requiredRP) * 100)}%`;
 }
 
-for (let i = 0; rpAfterGain > RPRequired; i++) {
-  rankedLevels++;
-  RPRequired = RPForMultipleLevels(currentRank, rpBeforeGain, rankedLevels);
-  if (rpAfterGain < RPRequired) {
-    rankedLevels--;
-    console.log(`Stopped\nExtra lvls: ${rankedLevels}`);
-    break;
+function checkRank(currentRank, currentRP, addedRP) {
+  let addedLevels = 0;
+  let hasRankedUp = false;
+
+  let requiredRP = RPRequiredToLevelUp(currentRank + addedLevels, currentRP);
+
+  if (addedRP > requiredRP) {
+    hasRankedUp = true;
+    addedLevels++;
   }
-  console.log(
-    `Extra Levels: ${rankedLevels} : Current Level: ${
-      currentRank + rankedLevels
-    } : XP for next: ${RPRequired}`
-  );
+
+  let remainingRP = addedRP - requiredRP;
+  if (Math.abs(remainingRP) === remainingRP) {
+    for (remainingRP; remainingRP > requiredRP; remainingRP -= requiredRP) {
+      addedLevels++;
+      requiredRP = RPRequiredToLevelUp(currentRank + addedLevels, currentRP);
+    }
+  }
+
+  return [hasRankedUp, addedLevels, remainingRP];
 }
 
-console.log(rankedLevels);
+console.log(checkRank(currentRank, currentRP, currentRP + earnedRP));
