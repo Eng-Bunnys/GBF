e580fe2ebd7f34fb65e93e03ea5d03f2cd8f09f7
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-02-23 13:37:58 +0200

    Merged fields

---
+++ GBF/Timer/User Profile Schema.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const UserProfileSchema = new Schema(
+  {
+    userID: String,
+    cash: {
+      type: Number,
+      default: 1500
+    },
+    dunkelCoins: {
+      type: Number,
+      default: 500
+    },
+    dailyCooldown: Date,
+    //type: Date,
+    //default: new Date(Date.now() - 86400000)
+    dailyStreak: Number
+  },
+  {
+    collection: "User Profile Data"
+  }
+);
+
+module.exports = model("User Profile Data", UserProfileSchema);
