35858dc1e182a6aa50ce692adc6ba849e250d022
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-03-08 22:44:30 +0200

    Many bug fixes & UI updates

--- GBF/Timer/Daily.js
+++ GBF/Timer/Daily.js
@@ -39,7 +39,7 @@ module.exports = class DailyClaim extends SlashCommand {
       name: "daily",
       category: "Economy",
       description: "Login in daily to get cool free prizes",
-      devOnly: false,
+      devOnly: true,
       userPermission: [],
       botPermission: [],
       cooldown: 0,
@@ -48,136 +48,87 @@ module.exports = class DailyClaim extends SlashCommand {
   }
 
   async execute({ client, interaction }) {
-    // Checking if the user has a profile
-
-    // Checking for certain category completion such as semester start etc. is bad since this is a multi-category command
-
     const userData = await userSchema.findOne({
       userID: interaction.user.id
     });
 
-    const timerData = await timerSchema.find({
+    const timerData = await timerSchema.findOne({
       userID: interaction.user.id
     });
 
-    // There is a possibility of the user not having a timer account, so we need to check which one we use first
-
-    let usedTimerData;
-
-    // If the user does not have an account, we register one with no category based settings
-
-    const newAccount = new MessageEmbed()
-      .setTitle(`${emojis.MaxRank} Collected`)
-      .setColor(colours.DEFAULT)
-      .setDescription(
-        `Hey!\nThanks for logging in GBF today, as a thank you, you'll receive a reward based on the day of the week and your daily streak!\n\nI've given you an extra reward as a welcome gift 😉\n\n• 250 DunkelCoins ${emojis.dunkelCoin}\n• 5,000 Timer XP`
-      )
-      .setFooter({
-        text: `A Timer Account is required to claim the 5,000 XP, if you already own one, you won't get a level up message until a session ends.`
-      });
-
     if (!userData) {
-      const newData = new userSchema({
+      const newAccount = new MessageEmbed()
+        .setTitle(`Daily Collected ${emojis.MaxRank}`)
+        .setColor(colours.DEFAULT)
+        .setDescription(
+          `Hey!\nThanks for logging in GBF today, as a thank you, you'll receive a reward based on the day of the week and your daily streak!\n\nI've given you an extra reward as a welcome gift 😉\n\n• 250 DunkelCoins ${emojis.dunkelCoin}\n• 5,000 Timer XP`
+        )
+        .setFooter({
+          text: `A Timer Account is required to claim the 5,000 XP.`
+        });
+
+      const newUserProfile = new userSchema({
         userID: interaction.user.id,
-        dunkelCoins: 750,
-        dailyCooldown: new Date(Date.now()),
-        dailyStreak: 1
-      });
-
-      await newData.save();
-
-      return interaction.reply({
-        embeds: [newAccount]
-      });
-    }
-
-    if (!timerData) {
-      const newTimerData = new timerSchema({
-        userID: interaction.user.id,
-        seasonXP: 5000,
-        accountXP: 5000
-      });
-
-      await newTimerData.save();
-
-      usedTimerData = newTimerData;
-    }
-
-    if (timerData) {
-      await timerData.updateOne({
-        seasonXP: timerData.seasonXP + 5000,
-        accountXP: timerData.accountXP + 5000
-      });
-      usedTimerData = timerData;
-    }
-
-    if (userData && !userData.dailyCooldown) {
-      await userData.updateOne({
         dailyCooldown: new Date(Date.now()),
         dailyStreak: 1,
-        dunkelCoins: userData.dunkelCoins + 250
+        dunkelCoins: 750
       });
 
-      await usedTimerData.updateOne({
-        seasonXP: timerData.seasonXP + 5000,
-        accountXP: timerData.accountXP + 5000
-      });
+      if (timerData) {
+        await timerData.updateOne({
+          seasonXP: timerData.seasonXP + 5000,
+          accountXP: timerData.accountXP + 5000
+        });
+
+        const rankUpSeason = checkRank(
+          timerData.seasonLevel,
+          timerData.seasonXP,
+          timerData.seasonXP + 5000
+        );
+
+        const rankUpAccount = checkRankAccount(
+          timerData.accountLevel,
+          timerData.accountXP,
+          timerData.accountXP + 5000
+        );
+
+        console.log(rankUpAccount);
+
+        if (rankUpSeason[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "seasonLevel",
+            rankUpSeason[1],
+            rankUpSeason[2]
+          );
+        }
+
+        if (rankUpAccount[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "accountLevel",
+            rankUpAccount[1],
+            rankUpAccount[2]
+          );
+        }
+      } else {
+        await newUserProfile.updateOne({
+          extraTimerXP: 5000
+        });
+      }
+
+      await newUserProfile.save();
 
       return interaction.reply({
         embeds: [newAccount]
       });
     }
 
-    const rankUpSeason = checkRank(
-      usedTimerData.seasonLevel,
-      usedTimerData.seasonXP,
-      usedTimerData.seasonXP + 5000
-    );
-
-    const rankUpAccount = checkRankAccount(
-      usedTimerData.accountLevel,
-      usedTimerData.accountXP,
-      usedTimerData.accountXP + 5000
-    );
-
-    if (rankUpSeason[0])
-      await client.emit(
-        "playerLevelUp",
-        interaction,
-        usedTimerData,
-        "seasonLevel",
-        rankUpSeason[1],
-        rankUpSeason[2]
-      );
-
-    if (rankUpAccount[0])
-      await client.emit(
-        "playerLevelUp",
-        interaction,
-        usedTimerData,
-        "accountLevel",
-        rankUpAccount[1],
-        rankUpAccount[2]
-      );
-    // Checking if the user is on cooldown
-
-    const onCooldown = new MessageEmbed()
-      .setTitle(`⏰ You can't do that yet`)
-      .setColor(colours.DEFAULT)
-      .setDescription(
-        `You've already collected your daily login reward\n\nYou can collect it again <t:${Math.floor(
-          userData.dailyCooldown / 1000 + 86400
-        )}:R>.`
-      )
-      .setTimestamp();
-
-    if (Date.parse(userData.dailyCooldown) + 86400000 > Date.now())
-      return interaction.reply({
-        embeds: [onCooldown],
-        ephemeral: true
-      });
-
-    const currentStreak = userData.dailyStreak;
+    const currentStreak = userData.dailyStreak + 1;
 
     const lostStreak = new MessageEmbed()
       .setTitle(`Daily Collected 💰`)
@@ -187,7 +138,9 @@ module.exports = class DailyClaim extends SlashCommand {
           loginReward()[0]
         } / 7]\n\n• ${
           loginReward()[1]
-        } Timer XP\n\nYou failed to keep up the daily streak, your last streak was ${currentStreak.toLocaleString()} days.\nYou can collect your next daily: <t:${next24Hours}:F>`
+        } Timer XP\n\nYou failed to keep up the daily streak, your last streak was ${(
+          currentStreak - 1
+        ).toLocaleString()} days.\nYou can collect your next daily: <t:${next24Hours}:F>`
       )
       .setFooter({
         text: `If you think there is a mistake please contact support (/bot invite) || The streak dies after 48 hours of not claiming your reward`,
@@ -200,10 +153,158 @@ module.exports = class DailyClaim extends SlashCommand {
         dailyCooldown: new Date(Date.now())
       });
 
-      await usedTimerData.updateOne({
-        seasonXP: usedTimerData.seasonXP + Number(loginReward()[1]),
-        accountXP: usedTimerData.accountXP + Number(loginReward()[1])
+      if (timerData) {
+        await timerData.updateOne({
+          seasonXP: timerData.seasonXP + Number(loginReward()[1]),
+          accountXP: timerData.accountXP + Number(loginReward()[1])
+        });
+
+        const rankUpSeason = checkRank(
+          timerData.seasonLevel,
+          timerData.seasonXP,
+          timerData.seasonXP + Number(loginReward()[1])
+        );
+
+        const rankUpAccount = checkRankAccount(
+          timerData.accountLevel,
+          timerData.accountXP,
+          timerData.accountXP + Number(loginReward()[1])
+        );
+
+        if (rankUpSeason[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "seasonLevel",
+            rankUpSeason[1],
+            rankUpSeason[2]
+          );
+        }
+
+        if (rankUpAccount[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "accountLevel",
+            rankUpAccount[1],
+            rankUpAccount[2]
+          );
+        }
+      } else {
+        await userData.updateOne({
+          extraTimerXP: userData.extraTimerXP + Number(loginReward()[1])
+        });
+      }
+
+      return interaction.reply({
+        embeds: [lostStreak]
       });
     }
+
+    const onCooldown = new MessageEmbed()
+      .setTitle(`⏰ You can't do that yet`)
+      .setColor(colours.DEFAULT)
+      .setDescription(
+        `You've already collected your daily login reward [Day: ${
+          loginReward(currentStreak)[0]
+        }]\n\nYou can collect it again <t:${Math.floor(
+          userData.dailyCooldown / 1000 + 86400
+        )}:R>.`
+      )
+      .setTimestamp();
+
+    if (Date.parse(userData.dailyCooldown) + 86400000 > Date.now())
+      return interaction.reply({
+        embeds: [onCooldown],
+        ephemeral: true
+      });
+
+    const dailyReward =
+      loginReward(currentStreak)[1] != 0
+        ? `${loginReward(currentStreak)[1].toLocaleString()} Timer XP`
+        : `${loginReward(currentStreak)[2].toLocaleString()} DunkelCoins ${
+            emojis.dunkelCoin
+          }`;
+
+    const collectedReward = new MessageEmbed()
+      .setTitle(`Daily Collected 💰`)
+      .setColor(colours.DEFAULT)
+      .setDescription(
+        `Collected your daily login reward [Day: ${
+          loginReward(currentStreak)[0]
+        } / 7]\n\n• ${dailyReward} , You can collect your next daily: <t:${next24Hours}:F>.`
+      )
+      .setTimestamp();
+
+    await userData.updateOne({
+      dailyCooldown: new Date(Date.now()),
+      dailyStreak: userData.dailyStreak + 1,
+      dunkelCoins:
+        loginReward(currentStreak)[2] != 0
+          ? userData.dunkelCoins + loginReward(currentStreak)[2]
+          : userData.dunkelCoins
+    });
+
+    if (timerData) {
+      await timerData.updateOne({
+        seasonXP:
+          loginReward(currentStreak)[1] != 0
+            ? timerData.seasonXP + loginReward(currentStreak)[1]
+            : timerData.seasonXP,
+        accountXP:
+          loginReward(currentStreak)[1] != 0
+            ? timerData.accountXP + loginReward(currentStreak)[1]
+            : timerData.accountXP
+      });
+
+      if (loginReward(currentStreak)[1] != 0) {
+        const rankUpSeason = checkRank(
+          timerData.seasonLevel,
+          timerData.seasonXP,
+          timerData.seasonXP + Number(loginReward(currentStreak)[1])
+        );
+
+        const rankUpAccount = checkRankAccount(
+          timerData.accountLevel,
+          timerData.accountXP,
+          timerData.accountXP + Number(loginReward(currentStreak)[1])
+        );
+
+        if (rankUpSeason[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "seasonLevel",
+            rankUpSeason[1],
+            rankUpSeason[2]
+          );
+        }
+
+        if (rankUpAccount[0]) {
+          await client.emit(
+            "playerLevelUp",
+            interaction,
+            timerData,
+            "accountLevel",
+            rankUpAccount[1],
+            rankUpAccount[2]
+          );
+        }
+      }
+    } else {
+      await userData.updateOne({
+        extraTimerXP:
+          loginReward(currentStreak)[1] != 0
+            ? userData.extraTimerXP + loginReward(currentStreak)[1]
+            : userData.extraTimerXP
+      });
+    }
+
+    return interaction.reply({
+      embeds: [collectedReward]
+    });
   }
 };
