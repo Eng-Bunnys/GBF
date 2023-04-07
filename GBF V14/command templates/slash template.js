6cf043c797ce821a971cbd52c4e446f0314c8635
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-04-07 15:05:45 +0200

    Slash command initiation template

---
+++ GBF V14/command templates/slash template.js
@@ -0,0 +1,34 @@
+const SlashCommand = require('../utils/slashCommands');
+
+module.exports = class Name extends SlashCommand {
+  constructor(client) {
+    super(client, {
+      name: "name",
+      category: "category",
+      description: "description",
+      usage: "usage",
+      examples: "example",
+
+      options: [{
+        name: "text",
+        description: "Hello World",
+        type: "",
+        required: true
+      }],
+
+      devOnly: true,
+      userPermission: [],
+      botPermission: [],
+      cooldown: 0,
+      development: true,
+      Partner: true,
+    });
+  }
+
+  async execute({
+    client,
+    interaction
+  }) {
+
+  }
+}
