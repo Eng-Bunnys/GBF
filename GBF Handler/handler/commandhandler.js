62e24432337ee1a3f3fd3363c35e79bcef6c1716
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-04-25 03:24:32 +0200

    Added interface

--- GBF Handler/handler/commandhandler.js
+++ GBF Handler/handler/commandhandler.js
@@ -1,35 +1,70 @@
 class GBFCmd {
+  readonly client: any;
+  readonly name: string;
+  readonly aliases: string[];
+  readonly category: string;
+  readonly description: string;
+  readonly usage: string;
+  readonly examples: string;
+  readonly cooldown: number;
+  readonly userPermission: string[];
+  readonly botPermission: string[];
+  readonly devOnly: boolean;
+  readonly development: boolean;
+  readonly devBypass: boolean;
+  readonly Partner: boolean;
+  readonly args: any[];
 
-    constructor (client, {
-        name = "",
-        aliases = [],
-        category = "",
-        description = "",
-        usage = "",
-        examples = "",
-        cooldown = 0,
-        userPermission = [],
-        botPermission = [],
-        devOnly = false,
-        Partner = false,
-        args = [],
-    }) {
-        this.client = client;
-        this.name = name;
-        this.aliases = aliases;
-        this.category = category;
-        this.description = description;
-        this.usage = usage;
-        this.examples = examples;
-        this.cooldown = cooldown;
-        this.userPermission = userPermission;
-        this.botPermission = botPermission;
-        this.devOnly = devOnly;
-        this.Partner = Partner
-        this.args = args;
+  constructor(
+    client: any,
+    {
+      name = "",
+      aliases = [],
+      category = "",
+      description = "",
+      usage = "",
+      examples = "",
+      cooldown = 0,
+      userPermission = [],
+      botPermission = [],
+      devOnly = false,
+      devBypass = false,
+      development = false,
+      Partner = false,
+      args = []
+    }: {
+      name?: string;
+      aliases?: string[];
+      category?: string;
+      description?: string;
+      usage?: string;
+      examples?: string;
+      cooldown?: number;
+      userPermission?: string[];
+      botPermission?: string[];
+      devOnly?: boolean;
+      devBypass?: boolean;
+      development?: boolean;
+      Partner?: boolean;
+      args?: any[];
     }
-
-   
+  ) {
+    this.client = client;
+    this.name = name;
+    this.aliases = aliases;
+    this.category = category;
+    this.description = description;
+    this.usage = usage;
+    this.examples = examples;
+    this.cooldown = cooldown;
+    this.userPermission = userPermission;
+    this.botPermission = botPermission;
+    this.devOnly = devOnly;
+    this.devBypass = devBypass;
+    this.development = development;
+    this.Partner = Partner;
+    this.args = args;
+  }
 }
 
-module.exports = GBFCmd;
+export = GBFCmd;
