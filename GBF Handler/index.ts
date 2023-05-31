33c9a4d3c112a5658d99c3343c7cfc38f2d16988
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-05-31 13:06:24 +0300

    Added new options

--- GBF Handler/index.ts
+++ GBF Handler/index.ts
@@ -1,13 +1,16 @@
 import { GatewayIntentBits } from "discord.js";
 import GBFClient from "./utils/client";
 import path from "path";
+import { DefaultCommands } from "./handler/clienthandler";
 const { TOKEN } = require(path.join(__dirname, "./config/GBFconfig.json"));
 
 export const client = new GBFClient({
+  Version: "2.5.0",
   CommandsFolder: "../commands",
   EventsFolder: "../events",
   Prefix: "!!",
-  SupportServer: "",
+  LogActions: true,
+  SupportServer: "https://discord.gg/yrM7fhgNBW",
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
@@ -17,12 +20,25 @@ export const client = new GBFClient({
     GatewayIntentBits.GuildEmojisAndStickers,
     GatewayIntentBits.GuildWebhooks
   ],
-  HelpMenu: true,
-  IgnoredHelpCategories: [],
+  IgnoredHelpCategories: ["Developer", "Storymode"],
   config: path.join(__dirname, "./config/GBFconfig.json"),
-  Developers: [],
-  TestServers: [],
-  Partners: []
+  Developers: ["333644367539470337", "841854342255345664"],
+  TestServers: [
+    "827589582932410388",
+    "850005402863140895",
+    "800076239490252810",
+    "439890528583286784"
+  ],
+  Partners: [
+    "333644367539470337",
+    "841854342255345664",
+    "320914878724571136",
+    "339822247613300746",
+    "365647018393206785",
+    "422072784131194880",
+    "539058806358016030"
+  ],
+  DisabledCommands: ["love"]
 });
 
 (async () => {
