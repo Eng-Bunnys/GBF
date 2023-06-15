e8abe2b1018e567616fa735203e1ad9552b1f029
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-15 21:13:27 +0300

    Delete Snippets directory

--- Snippets/ChangeRolePermissions.js
+++
@@ -1,7 +0,0 @@
-//Role permissions might be a bit confusing at first
-//To add or remove a permission you use the .remove or .add but not by itself
-//We do:
-//To remove a permission
-await role.setPermissions(role.permissions.remove(Permissions.FLAGS.<Permission>));
-//To add a permission
-await role.setPermissions(role.permissions.add(Permissions.FLAGS.<Permission>));                          
--- Snippets/totalBotUsers.js
+++
@@ -1,3 +0,0 @@
-// The cache counts every user uniquely hence not counting the total number of users in all guilds, hence we use <Guild>#memberCount method
-
-const totalUsers = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
