085adad6df94ce24f4c0e3e4ced124535a5e780c
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-24 23:33:09 +0300

    Added Freebie in client presence

--- GBF V14/events/Bot/ready.ts
+++ GBF V14/events/Bot/ready.ts
@@ -1,22 +1,45 @@
-import chalk, { redBright, whiteBright, underline } from "chalk";
+import { redBright, whiteBright, underline } from "chalk";
 
-import figlet, { textSync } from "figlet";
+import { textSync } from "figlet";
 
 import { capitalize } from "../../utils/Engine";
 
-import { Client, Events } from "discord.js";
+import { ActivityType, Events } from "discord.js";
+import GBFClient from "../../handler/clienthandler";
+import {
+  NumberOfFreebies,
+  GameTitle,
+  GameTwoTitle
+} from "../../GBF/Freebies/Game Settings/Epic Games Settings.json";
 
-import GBFVersion from "../../GBF/Version.json";
-
-export default function botReady(client: Client) {
+export default function botReady(client: GBFClient) {
   client.on(Events.ClientReady, async () => {
-    const totalUsers = client.guilds.cache.reduce(
-      (acc, guild) => acc + guild.memberCount,
-      0
-    );
+    const totalUsers =
+      client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+
+    let FreebiePresence: string;
+    let PresenceType:
+      | ActivityType.Playing
+      | ActivityType.Streaming
+      | ActivityType.Listening
+      | ActivityType.Watching
+      | ActivityType.Competing;
+
+    if (NumberOfFreebies === 1) {
+      FreebiePresence = `${GameTitle}, free on Epic Games!`;
+      PresenceType = ActivityType.Streaming;
+    }
+    if (NumberOfFreebies === 2) {
+      FreebiePresence = `${GameTitle} & ${GameTwoTitle} are Free on Epic Games!`;
+      PresenceType = ActivityType.Watching;
+    }
+    if (NumberOfFreebies === 3) {
+      FreebiePresence = `3 Free Games on Epic Games!`;
+      PresenceType = ActivityType.Watching;
+    }
 
     client.user.setPresence({
-      activities: [{ name: "GBF 2.5.0", type: 2 }],
+      activities: [{ name: FreebiePresence, type: PresenceType }],
       status: "online"
     });
 
@@ -30,9 +53,7 @@ export default function botReady(client: Client) {
 
     console.log(
       redBright(
-        underline(
-          `${client.user.username} is now online! v${GBFVersion.Version}`
-        )
+        underline(`${client.user.username} is now online! v${client.Version}`)
       )
     );
 
@@ -59,8 +80,6 @@ export default function botReady(client: Client) {
   });
 
   process.on("uncaughtException", (err: Error, origin: string) => {
-    console.log(
-      `Caught uncaughtException: ${err}\n` + `Exception origin: ${origin}`
-    );
+    console.log(`Caught uncaughtException: ${err}`);
   });
 }
