7debf613784d2d53a6ed54554330bb980e37dd1a
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-25 11:26:52 +0300

    Simplified how it works

--- GBF Handler/utils/context.ts
+++ GBF Handler/utils/context.ts
@@ -1,28 +1,8 @@
-import { CacheType, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction } from "discord.js";
 import GBFClient from "../handler/clienthandler";
-import { CommandOptions, GBFCmd } from "../handler/commandhandler";
+import { GBFCtx, GBFCtxOptions } from "../handler/contextHandler";
 
-interface CtxMenuExecute<T> {
-    execute(i: T): unknown
-
-}
-
-export class ContextMessageCommand extends GBFCmd implements CtxMenuExecute<MessageContextMenuCommandInteraction> {
-  constructor(client: GBFClient, options: CommandOptions) {
+export class ContextCommand extends GBFCtx {
+  constructor(client: GBFClient, options: GBFCtxOptions) {
     super(client, options);
   }
-  execute(i: MessageContextMenuCommandInteraction<CacheType>): unknown {
-        throw new Error("Extend & override this class to execute");
-   }
-
-}
-
-export class ContextUserCommand extends GBFCmd implements CtxMenuExecute<UserContextMenuCommandInteraction>{
-
-  constructor(client: GBFClient, options: CommandOptions)  {
-    super(client, options);
-  }
-    execute(i: UserContextMenuCommandInteraction<CacheType>): unknown {
-        throw new Error("Extend & override this class to execute");
-    }
 }
