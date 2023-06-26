6c44e74460013d1ada82b035e3bc961513a9ffb4
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-27 02:20:23 +0300

    Context menu support

--- GBF Handler/handler/Handler Events/interactionCreate.ts
+++ GBF Handler/handler/Handler Events/interactionCreate.ts
@@ -5,7 +5,6 @@ import {
   EmbedBuilder,
   Events,
   GuildMember,
-  Interaction,
   PermissionResolvable,
   TextChannel
 } from "discord.js";
@@ -25,47 +24,25 @@ import {
   IGuildData
 } from "../../schemas/GBF Schemas/Guild Data Schema";
 import GBFClient from "../clienthandler";
-import { GBFCtxOptions } from "../contextHandler";
 
 const cooldowns = new Collection();
 
-export default function interactionCreate(client: GBFClient) {
-  client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-    const blackListData = await GBFBotBanModel.findOne({
-      userId: interaction.user.id
-    });
-
-    let guildData: IGuildData | null;
-
-    if (!interaction.inGuild()) guildData = null;
-    else
-      guildData = await GBFGuildDataModel.findOne({
-        guildID: interaction.guild.id
+export default function interactionCreate(client) {
+  client.on(
+    Events.InteractionCreate,
+    async (interaction: CommandInteraction) => {
+      const blackListData = await GBFBotBanModel.findOne({
+        userId: interaction.user.id
       });
 
-    const PartnerOnly = new EmbedBuilder()
-      .setTitle(`${emojis.ERROR} You can't use that`)
-      .setDescription(`This command is only available for partners.`)
-      .setColor(colours.ERRORRED as ColorResolvable);
+      let guildData: IGuildData | null;
 
-    const DevOnly = new EmbedBuilder()
-      .setTitle(`${emojis.ERROR} You can't use that`)
-      .setDescription("This command is only available for developers.")
-      .setColor(colours.ERRORRED as ColorResolvable);
+      if (!interaction.inGuild()) guildData = null;
+      else
+        guildData = await GBFGuildDataModel.findOne({
+          guildID: interaction.guild.id
+        });
 
-    const DisabledCommand = new EmbedBuilder()
-      .setTitle(`${emojis.ERROR} You can't do that`)
-      .setColor(colours.ERRORRED as ColorResolvable)
-      .setDescription(`This command is disabled in ${interaction.guild.name}`);
-
-    const NSFWDisabled = new EmbedBuilder()
-      .setTitle(`${emojis.ERROR} You can't do that`)
-      .setColor(colours.ERRORRED as ColorResolvable)
-      .setDescription(
-        `This is an age-restricted command, this can only be used in age-restricted channels.`
-      );
-
-    if (interaction.isCommand()) {
       const suspendedEmbed = new EmbedBuilder()
         .setTitle(`${emojis.ERROR} You can't do that`)
         .setDescription(
@@ -79,8 +56,18 @@ export default function interactionCreate(client: GBFClient) {
           ephemeral: true
         });
 
-      if (interaction.isContextMenuCommand()) {
-        const command: GBFCtxOptions = client.contextCmds.get(
+      const PartnerOnly = new EmbedBuilder()
+        .setTitle(`${emojis.ERROR} You can't use that`)
+        .setDescription(`This command is only available for partners.`)
+        .setColor(colours.ERRORRED as ColorResolvable);
+
+      const DevOnly = new EmbedBuilder()
+        .setTitle(`${emojis.ERROR} You can't use that`)
+        .setDescription("This command is only available for developers.")
+        .setColor(colours.ERRORRED as ColorResolvable);
+
+      if (interaction.isContextMenuCommand) {
+        const command: GBFSlashOptions = client.slashCommands.get(
           interaction.commandName
         );
 
@@ -98,132 +85,13 @@ export default function interactionCreate(client: GBFClient) {
 
         if (
           command.devOnly &&
-          !client.Developers.includes(interaction.user.id)
+          !(client as GBFClient).Developers.includes(interaction.user.id)
         ) {
           return interaction.reply({
             embeds: [DevOnly],
             ephemeral: true
           });
         }
-
-        if (guildData && guildData.DisabledCommands.includes(command.name))
-          return interaction.reply({
-            embeds: [DisabledCommand],
-            ephemeral: true
-          });
-
-        if (
-          command.NSFW &&
-          interaction.inGuild() &&
-          !(interaction.channel as TextChannel).nsfw
-        )
-          return interaction.reply({
-            embeds: [NSFWDisabled],
-            ephemeral: true
-          });
-
-        if (
-          !command.devBypass ||
-          (command.devBypass &&
-            !(client as GBFClient).Developers.includes(interaction.user.id))
-        ) {
-          const cooldownAmountS = command.cooldown;
-          if (cooldownAmountS) {
-            if (!cooldowns.has(command.name))
-              cooldowns.set(command.name, new Collection());
-
-            const now = Date.now();
-            const timestamps: any = cooldowns.get(command.name);
-            const cooldownAmountMS = cooldownAmountS * 1000;
-
-            if (timestamps.has(interaction.user.id)) {
-              const expirationTime =
-                timestamps.get(interaction.user.id) + cooldownAmountMS;
-
-              if (now < expirationTime) {
-                const timeLeft = Number((expirationTime - now).toFixed(1));
-                const cooldownembed = new EmbedBuilder()
-                  .setDescription(
-                    `${interaction.user}, please wait ${msToTime(
-                      Number(timeLeft)
-                    )} before reusing the \`${capitalize(
-                      command.name
-                    )}\` command.`
-                  )
-                  .setColor(colours.ERRORRED as ColorResolvable);
-
-                return interaction.reply({
-                  embeds: [cooldownembed],
-                  ephemeral: true
-                });
-              }
-            }
-            timestamps.set(interaction.user.id, now);
-            setTimeout(
-              () => timestamps.delete(interaction.user.id),
-              cooldownAmountMS
-            );
-          }
-        }
-
-        if (!command.dmEnabled && !interaction.inGuild()) {
-          const dmDisabled = new EmbedBuilder()
-            .setTitle(`${emojis.ERROR} You can't do that`)
-            .setColor(colours.ERRORRED as ColorResolvable)
-            .setDescription(`${capitalize(command.name)} is disabled in DMs.`);
-
-          return (interaction as CommandInteraction).reply({
-            embeds: [dmDisabled]
-          });
-        }
-
-        if (interaction.inGuild()) {
-          if (
-            command.botPermission &&
-            !interaction.channel
-              .permissionsFor(interaction.guild.members.me)
-              .has(command.botPermission as PermissionResolvable, true)
-          ) {
-            const missingPermBot = new EmbedBuilder()
-              .setTitle("Missing Permission")
-              .setDescription(
-                `I am missing the following permissions: ${missingPermissions(
-                  interaction.guild.members.me,
-                  command.botPermission
-                )}`
-              )
-              .setColor(colours.ERRORRED as ColorResolvable);
-
-            return interaction.reply({
-              embeds: [missingPermBot],
-              ephemeral: true
-            });
-          }
-
-          if (
-            command.userPermission &&
-            !(interaction.member as GuildMember).permissions.has(
-              command.userPermission as PermissionResolvable,
-              true
-            )
-          ) {
-            const missingPermUser = new EmbedBuilder()
-              .setTitle("Missing Permissions")
-              .setDescription(
-                `You are missing the following permissions: ${missingPermissions(
-                  interaction.member,
-                  command.userPermission
-                )}`
-              )
-              .setColor(colours.ERRORRED as ColorResolvable);
-
-            return interaction.reply({
-              embeds: [missingPermUser],
-              ephemeral: true
-            });
-          }
-        }
-        
 
         try {
           //@ts-ignore
@@ -232,7 +100,7 @@ export default function interactionCreate(client: GBFClient) {
             interaction
           });
         } catch (err) {
-          console.log(err);
+          console.log(`Error running command '${command.name}'\n${err}`);
         }
       }
 
@@ -255,7 +123,7 @@ export default function interactionCreate(client: GBFClient) {
 
         if (
           command.devOnly &&
-          !client.Developers.includes(interaction.user.id)
+          !(client as GBFClient).Developers.includes(interaction.user.id)
         ) {
           return interaction.reply({
             embeds: [DevOnly],
@@ -263,19 +131,16 @@ export default function interactionCreate(client: GBFClient) {
           });
         }
 
+        const DisabledCommand = new EmbedBuilder()
+          .setTitle(`${emojis.ERROR} You can't do that`)
+          .setColor(colours.ERRORRED as ColorResolvable)
+          .setDescription(
+            `This command is disabled in ${interaction.guild.name}`
+          );
+
         if (guildData && guildData.DisabledCommands.includes(command.name))
           return interaction.reply({
             embeds: [DisabledCommand],
-            ephemeral: true
-          });
-
-        if (
-          command.NSFW &&
-          interaction.inGuild() &&
-          !(interaction.channel as TextChannel).nsfw
-        )
-          return interaction.reply({
-            embeds: [NSFWDisabled],
             ephemeral: true
           });
 
@@ -321,17 +186,6 @@ export default function interactionCreate(client: GBFClient) {
               cooldownAmountMS
             );
           }
-        }
-
-        if (!command.dmEnabled && !interaction.inGuild()) {
-          const dmDisabled = new EmbedBuilder()
-            .setTitle(`${emojis.ERROR} You can't do that`)
-            .setColor(colours.ERRORRED as ColorResolvable)
-            .setDescription(`${capitalize(command.name)} is disabled in DMs.`);
-
-          return (interaction as CommandInteraction).reply({
-            embeds: [dmDisabled]
-          });
         }
 
         if (interaction.inGuild()) {
@@ -398,7 +252,6 @@ export default function interactionCreate(client: GBFClient) {
                 subcommand
               });
           }
-
           //@ts-ignore
           await command.execute({
             client,
@@ -411,5 +264,5 @@ export default function interactionCreate(client: GBFClient) {
         }
       }
     }
-  });
+  );
 }
