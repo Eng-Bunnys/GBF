2fe8e19ca9ea00a9970e63502659085731712114
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-27 04:37:24 +0300

    Delete context.ts

--- GBF Handler/utils/context.ts
+++
@@ -1,8 +0,0 @@
-import GBFClient from "../handler/clienthandler";
-import { GBFCtx, GBFCtxOptions } from "../handler/contextHandler";
-
-export class ContextCommand extends GBFCtx {
-  constructor(client: GBFClient, options: GBFCtxOptions) {
-    super(client, options);
-  }
-}
