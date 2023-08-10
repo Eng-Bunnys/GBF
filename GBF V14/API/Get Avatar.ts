90047ec6d193c3039e8c9cd1fcb6e094f5ce5478
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-08-10 20:11:52 +0300

    GBF API Avatar

---
+++ GBF V14/API/Get Avatar.ts
@@ -0,0 +1,100 @@
+import {
+  User,
+  GuildMember,
+  ImageURLOptions,
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+  ColorResolvable,
+  ButtonStyle
+} from "discord.js";
+import { capitalize } from "../utils/Engine";
+import colors from "../GBF/GBFColor.json";
+
+class AvatarBuilder {
+  private readonly targetUser: User;
+  private readonly targetMember: GuildMember | undefined;
+  private readonly color: ColorResolvable;
+
+  constructor(
+    targetUser: User,
+    targetMember?: GuildMember,
+    color: ColorResolvable = colors.DEFAULT as ColorResolvable
+  ) {
+    this.targetUser = targetUser;
+    this.targetMember = targetMember;
+    this.color = color;
+  }
+
+  private getAvatarURLs(): string {
+    const imageSettings: ImageURLOptions = {
+      extension: "png",
+      size: 1024
+    };
+
+    let avatarURLs = `[Global Avatar](${this.targetUser.displayAvatarURL(
+      imageSettings
+    )})`;
+
+    if (
+      this.targetMember &&
+      this.targetMember.displayAvatarURL() !==
+        this.targetUser.displayAvatarURL()
+    ) {
+      avatarURLs += ` | [Server Avatar](${this.targetMember.displayAvatarURL(
+        imageSettings
+      )})`;
+    }
+
+    return avatarURLs;
+  }
+
+  public getEmbed(): EmbedBuilder {
+    const avatarEmbed = new EmbedBuilder()
+      .setTitle(`${capitalize(this.targetUser.username)}'s Avatar`)
+      .setColor(this.color)
+      .setImage(
+        this.targetUser.displayAvatarURL({ extension: "png", size: 1024 })
+      )
+      .setDescription(this.getAvatarURLs());
+
+    if (this.targetMember) {
+      avatarEmbed.setThumbnail(
+        this.targetMember.displayAvatarURL({ extension: "png", size: 1024 })
+      );
+    }
+
+    return avatarEmbed;
+  }
+
+  public getButtonRow(): ActionRowBuilder {
+    const avatarButton = new ButtonBuilder()
+      .setLabel("Global Avatar")
+      .setStyle(ButtonStyle.Link)
+      .setURL(
+        this.targetUser.displayAvatarURL({ extension: "png", size: 1024 })
+      );
+
+    const buttonRow: ActionRowBuilder<any> =
+      new ActionRowBuilder().addComponents(avatarButton);
+
+    if (
+      this.targetMember &&
+      this.targetMember.displayAvatarURL() !==
+        this.targetUser.displayAvatarURL()
+    ) {
+      const serverAvatarButton = new ButtonBuilder()
+        .setLabel("Server Avatar URL")
+        .setStyle(ButtonStyle.Link)
+        .setURL(
+          this.targetMember.displayAvatarURL({ extension: "png", size: 1024 })
+        );
+
+      buttonRow.addComponents(serverAvatarButton);
+    }
+
+    return buttonRow;
+  }
+}
+
+export default AvatarBuilder;
