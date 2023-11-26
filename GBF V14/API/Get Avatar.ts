b0a364735d77ff8d7a3f47b2892de294538c1726
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-11-26 17:48:33 +0200

    Added priority & performance improvements

--- GBF V14/API/Get Avatar.ts
+++ GBF V14/API/Get Avatar.ts
@@ -11,29 +11,29 @@ import {
 import { capitalize } from "../utils/Engine";
 import colors from "../GBF/GBFColor.json";
 
+const ImageData: ImageURLOptions = { extension: "png", size: 1024 };
+
 class AvatarBuilder {
   private readonly targetUser: User;
   private readonly targetMember: GuildMember | undefined;
   private readonly color: ColorResolvable;
+  private readonly priority: boolean;
 
   constructor(
     targetUser: User,
     targetMember?: GuildMember,
-    color: ColorResolvable = colors.DEFAULT as ColorResolvable
+    color: ColorResolvable = colors.DEFAULT as ColorResolvable,
+    priority?: boolean
   ) {
     this.targetUser = targetUser;
     this.targetMember = targetMember;
     this.color = color;
+    this.priority = priority !== undefined ? priority : false;
   }
 
   private getAvatarURLs(): string {
-    const imageSettings: ImageURLOptions = {
-      extension: "png",
-      size: 1024,
-    };
-
     let avatarURLs = `[Global Avatar](${this.targetUser.displayAvatarURL(
-      imageSettings
+      ImageData
     )})`;
 
     if (
@@ -42,7 +42,7 @@ class AvatarBuilder {
         this.targetUser.displayAvatarURL()
     ) {
       avatarURLs += ` | [Server Avatar](${this.targetMember.displayAvatarURL(
-        imageSettings
+        ImageData
       )})`;
     }
 
@@ -50,54 +50,45 @@ class AvatarBuilder {
   }
 
   public getEmbed(): EmbedBuilder {
-    const avatarEmbed = new EmbedBuilder()
+    const AvatarEmbed = new EmbedBuilder()
       .setTitle(`${capitalize(this.targetUser.username)}'s Avatar`)
       .setColor(this.color)
-      .setImage(
-        this.targetUser.displayAvatarURL({ extension: "png", size: 1024 })
-      )
       .setDescription(this.getAvatarURLs());
 
-    if (
-      this.targetMember &&
-      this.targetMember.displayAvatarURL() !==
-        this.targetUser.displayAvatarURL()
-    ) {
-      avatarEmbed.setThumbnail(
-        this.targetMember.displayAvatarURL({ extension: "png", size: 1024 })
-      );
+    if (this.priority && this.targetMember) {
+      AvatarEmbed.setImage(
+        this.targetMember.displayAvatarURL(ImageData)
+      ).setThumbnail(this.targetUser.displayAvatarURL(ImageData));
+    } else {
+      AvatarEmbed.setImage(this.targetUser.displayAvatarURL(ImageData));
     }
 
-    return avatarEmbed;
+    return AvatarEmbed;
   }
 
   public getButtonRow(): ActionRowBuilder {
-    const avatarButton = new ButtonBuilder()
+    const AvatarButtons = new ButtonBuilder()
       .setLabel("Global Avatar")
       .setStyle(ButtonStyle.Link)
-      .setURL(
-        this.targetUser.displayAvatarURL({ extension: "png", size: 1024 })
-      );
+      .setURL(this.targetUser.displayAvatarURL(ImageData));
 
-    const buttonRow: ActionRowBuilder<any> =
-      new ActionRowBuilder().addComponents(avatarButton);
+    const AvatarButtonsRow: ActionRowBuilder<any> =
+      new ActionRowBuilder().addComponents(AvatarButtons);
 
     if (
       this.targetMember &&
       this.targetMember.displayAvatarURL() !==
         this.targetUser.displayAvatarURL()
     ) {
-      const serverAvatarButton = new ButtonBuilder()
+      const ServerAvatarButton = new ButtonBuilder()
         .setLabel("Server Avatar URL")
         .setStyle(ButtonStyle.Link)
-        .setURL(
-          this.targetMember.displayAvatarURL({ extension: "png", size: 1024 })
-        );
+        .setURL(this.targetMember.displayAvatarURL(ImageData));
 
-      buttonRow.addComponents(serverAvatarButton);
+      AvatarButtonsRow.addComponents(ServerAvatarButton);
     }
 
-    return buttonRow;
+    return AvatarButtonsRow;
   }
 }
 
