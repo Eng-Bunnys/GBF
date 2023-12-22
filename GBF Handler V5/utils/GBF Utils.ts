583f7e5ac83468bc409ac749ab0d742aa30b4927
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-12-22 16:07:37 +0200

    Updated GBF Utils

---
+++ GBF Handler V5/utils/GBF Utils.ts
@@ -0,0 +1,81 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import dotenv from "dotenv";
+import { EmbedBuilder } from "discord.js";
+import { ColorCodes, Emojis } from "./GBF Features";
+dotenv.config();
+
+export type GBFErrors = "BotBanned" | "testTwo";
+
+interface Config {
+  MongoURI: string;
+  TOKEN: string;
+}
+
+export class GBFUtils {
+  protected ErrorEmbeds(ErrorType: GBFErrors) {
+    const ErrorEmbed = new EmbedBuilder()
+      .setColor(ColorCodes.ErrorRed)
+      .setTimestamp();
+
+    if (ErrorType === "BotBanned") {
+      ErrorEmbed.setTitle(`${Emojis.Error}`);
+    }
+  }
+  
+  static readFilesRecursively(
+    dir: string,
+    fileExtensions: string[] = []
+  ): string[] {
+    const files: string[] = [];
+
+    function readDirRecursive(currentDir: string) {
+      const entries = fs.readdirSync(currentDir, { withFileTypes: true });
+
+      entries.forEach((entry) => {
+        const filePath = path.join(currentDir, entry.name);
+
+        if (entry.isDirectory()) {
+          readDirRecursive(filePath);
+        } else {
+          const extension = path.extname(filePath).toLowerCase();
+
+          if (!fileExtensions || fileExtensions.includes(extension)) {
+            files.push(filePath);
+          }
+        }
+      });
+    }
+
+    readDirRecursive(dir);
+
+    return files;
+  }
+
+  static loadConfig(customConfigPath?: string): Config {
+    const ConfigPath =
+      customConfigPath || path.join(__dirname, "config/config.json");
+
+    try {
+      const jsonConfig = require(ConfigPath);
+      return {
+        ...jsonConfig,
+        MongoURI: jsonConfig.MONGO_URI,
+        TOKEN: jsonConfig.TOKEN,
+      };
+    } catch (error) {
+      if (customConfigPath) {
+        console.error(
+          `Error: Unable to load config from specified path: ${customConfigPath}`
+        );
+        process.exit(1);
+      }
+
+      return {
+        MongoURI: process.env.MONGO_URI,
+        TOKEN: process.env.TOKEN,
+      };
+    }
+  }
+}
