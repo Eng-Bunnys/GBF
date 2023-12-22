89b23f9faa974948a7c8b32071228e644199f8a5
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-12-23 00:29:09 +0200

    Added JSDocs Comments

--- GBF Handler V5/handler/Command Handlers/Message Handler.ts
+++ GBF Handler V5/handler/Command Handlers/Message Handler.ts
@@ -1,23 +1,36 @@
+// Import necessary types from Discord.js and your custom types
 import { Message } from "discord.js";
-import GBF from "../GBF";
+import { GBF } from "../GBF";
 import { CommandOptions } from "../types";
 
+/**
+ * Abstract class representing a message command.
+ *
+ * @template T - The type of arguments expected by the command.
+ */
 abstract class MessageCommand<T extends string[] | undefined = string[]> {
+  /**
+   * Creates a new instance of MessageCommand.
+   *
+   * @param {GBF} client - Your client instance.
+   * @param {CommandOptions<T>} options - Options for the command.
+   */
   constructor(
-    protected readonly client: GBF,
+    protected readonly client: GBF, // JS users: Replace with your actual client type.
     public readonly options: CommandOptions<T>
   ) {}
 
   /**
    * Execute the command.
    *
-   * @param client - Your client
-   * @param message - typeof Message
-   * @param args - The user input split into an array
-   * @returns A Promise resolving to a Discord message or any other result.
+   * @param {GBF} client - Your client instance.
+   * @param {Message} message - The Discord message.
+   * @param {T} args - The user input split into an array.
+   * @returns {Promise<Message | any>} - A Promise resolving to a Discord message or any other result.
+   * @abstract
    */
   abstract execute(
-    client: GBF,
+    client: GBF, // JS users: Replace with your actual client type.
     message: Message,
     args?: T
   ): Promise<Message | any>;
