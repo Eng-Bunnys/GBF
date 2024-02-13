71a831c52873c8557b29df5629ec0355c2bc6ade
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2024-02-13 11:09:44 +0200

    Remove need to use JSON for colors and emojis

---
+++ GBF/Utils/UI.ts
@@ -0,0 +1,16 @@
+export class GBFEmojis {
+  static readonly Verify = "✅";
+  static readonly Error = "❌";
+  static readonly GBFLogo = "<:LogoTransparent:838994085527945266>";
+}
+
+export class GBFColorCodes {
+  static readonly Default = "#e91e63";
+  static readonly ErrorRed = "#FF0000";
+  static readonly SuccessGreen = "#33a532";
+  static readonly SalmonPink = "#ff91a4";
+  static readonly CardinalRed = "#C41E3A";
+  static readonly Cherry = "#D2042D";
+  static readonly PastelRed = "#FAA0A0";
+  static readonly Cyan = "#00FFFF";
+}
