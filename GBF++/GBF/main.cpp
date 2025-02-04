dbb98464a34bbd6752beb91c0922f8b9cbe17f7d
.Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2025-02-04 16:35:42 +0200

    Create main.cpp

---
+++ GBF++/GBF/main.cpp
@@ -0,0 +1,30 @@
+// Handler Imports
+#include "Handler/GBF.h"
+#include "Utils/EnvReader/EnvReader.h"
+
+int main()
+{
+	try
+	{
+		EnvReader env(".env");
+		std::string botToken = env.get("TOKEN");
+
+		uint32_t intents = dpp::i_all_intents;
+
+		GBFOptions options(botToken, true, intents);
+
+		GBF client(options);
+
+		bool loggedIn = client.login();
+
+		if (!loggedIn)
+			return EXIT_FAILURE;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "I ran into an error\n"
+			<< e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
