e6863f56bc60c32b29ab1219eff5b17eb886115f
.Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2025-03-29 14:38:54 +0200

    Project cancelled

--- GBF Timers v2/README.md
+++
@@ -1,8 +0,0 @@
-# GBF Timers
-
-This is an upgrade to the website orignally made and abandoned
-
-Stack:
-- SpringBoot
-- React
-- Mongoose
--- GBF Timers v2/src/main/java/org/bunnys/timer/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package org.bunnys.timer.repositories;
-
-import org.bunnys.timer.models.Session;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository extends MongoRepository<Session, String> {
-}
